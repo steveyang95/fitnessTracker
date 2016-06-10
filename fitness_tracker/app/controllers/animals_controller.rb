343893e02e91c94740342dfeefc99dbe09d7cb55
steveyang95 <steveyang95@berkeley.edu>
2016-06-10 11:13:09 -0700

    Added basic animal logic/views

--- fitness_tracker/app/controllers/animals_controller.rb
+++ fitness_tracker/app/controllers/animals_controller.rb
@@ -1,15 +1,31 @@
 class AnimalsController < ApplicationController
-	belongs_to :babies
 	
   def new
+    @animal = Animal.new
   end
 
   def create
+    @animal = Animal.new(animal_params)    
+    if @animal.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def index
+    @animals = Animal.all
   end
 
   def show
+    @animal = Animal.find(params[:id])
   end
+
+  private
+
+  # A stronng params for Animal
+  def animal_params
+    params.require(:animal).permit(:name, :description, :height, :weight)
+  end
+
 end
