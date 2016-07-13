eca35a58085cbffac778b5763ae752ea6f2242e6
steveyang95 <steveyang95@berkeley.edu>
2016-07-12 17:40:38 -0700

    Allow for users to edit babies

--- fitness_tracker/config/routes.rb
+++ fitness_tracker/config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  # get 'animals/new'
-
-  # post 'animals/create'
-
-  # get 'animals/index'
-
-  # get 'animals/show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
