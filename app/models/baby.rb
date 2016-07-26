e8dd27d1ec3c2c7a0f9106faf7242a450ffb5f95
Jimmy <jimmy@glooko.com>
2016-07-25 21:09:44 -0700

    fix duplicate error messages

--- app/models/baby.rb
+++ app/models/baby.rb
@@ -1,6 +1,5 @@
 class Baby < ActiveRecord::Base
 	validates :first_name, :last_name, :height, :weight, :temperature, presence: true
-	validates :height, :weight, :temperature, numericality: true
 	validates :height, numericality: { less_than_or_equal_to: 70, greater_than_or_equal_to: 10 }
 	validates :weight, numericality: { less_than_or_equal_to: 11, greater_than_or_equal_to: 1 }
 	validates :temperature, numericality: { less_than_or_equal_to: 100, greater_than_or_equal_to: 20 }
